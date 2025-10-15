59d35be24605baf0752acef802ce085205bfc3f4
Hubert Zych <hzych@inf.ug.edu.pl>
2025-10-15 15:58:31 +0200

    lab1 zad3+4

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,10 +1,13 @@
 from src.account import Account
+import re
 
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", "09359462739")
+        account = Account("John", "Doe", "09359462739", "PROM_123")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert len(account.pesel) == 11
-        assert len(account.pesel) == 11
+        assert account.pesel == "09359462739"
+        assert len(account.pesel) == 11 or account.pesel == "Invalid"
+        assert account.promo_code == "PROM_123"
+        assert re.search("^PROM_.{3}$",account.promo_code) != None or account.promo_code is None
