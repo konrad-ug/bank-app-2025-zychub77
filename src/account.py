2dba9be03d65c2a785c7dd5abc75b282c87cf1bd
Hubert Zych <hzych@inf.ug.edu.pl>
2025-10-15 15:18:08 +0200

    lab1 zad1+2

--- src/account.py
+++ src/account.py
@@ -1,4 +1,6 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel):
         self.first_name = first_name
         self.last_name = last_name
+        self.balance = 0.0
+        self.pesel = pesel
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "09359462739")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert len(account.pesel) == 11
+        assert len(account.pesel) == 11
