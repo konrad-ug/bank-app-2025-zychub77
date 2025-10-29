ae8ec37f6fc81533b9082b18d4b47a8c745edf12
Hubert Zych <h.zych.233@studms.ug.edu.pl>
2025-10-29 15:46:09 +0100

    Feature 10 - test coverage fix test

--- tests/unit/test_company_account_create.py
+++ tests/unit/test_company_account_create.py
@@ -11,9 +11,9 @@ class TestCompanyAccount:
         account3 = CompanyAccount("lidl", "123")
         assert account3.NIP == "Invalid"
 
-    # def test_make_express_transfer(self):
-    #     account = CompanyAccount("żabka", "1234567890")
-    #     account2 = CompanyAccount("biedronka", "0987654321")
-    #     account.balance = 10
-    #     account.make_express_transfer(10, account2)
-    #     assert account.balance == -5.0
+    def test_make_express_transfer(self):
+        account = CompanyAccount("żabka", "1234567890")
+        account2 = CompanyAccount("biedronka", "0987654321")
+        account.balance = 10
+        account.make_express_transfer(10, account2)
+        assert account.balance == -5.0
