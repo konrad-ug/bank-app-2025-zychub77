596b312cb606564d29b83a9f4fae633b93fed624
Hubert Zych <h.zych.233@studms.ug.edu.pl>
2025-11-19 15:49:20 +0100

    Feature 12

--- tests/unit/test_company_account_create.py
+++ tests/unit/test_company_account_create.py
@@ -1,9 +1,19 @@
 from src.CompanyAccount import CompanyAccount
+import pytest
 
 
 class TestCompanyAccount:
-    def test_account_creation(self):
+    @pytest.fixture
+    def account(self):
         account = CompanyAccount("żabka", "1234567890")
+        return account
+
+    @pytest.fixture
+    def account2(self):
+        account2 = CompanyAccount("biedronka", "0987654321")
+        return account2
+
+    def test_account_creation(self, account):
         assert account.company_name == "żabka"
         assert account.NIP == "1234567890"
 
@@ -11,16 +21,24 @@ class TestCompanyAccount:
         account3 = CompanyAccount("lidl", "123")
         assert account3.NIP == "Invalid"
 
-    def test_make_express_transfer(self):
-        account = CompanyAccount("żabka", "1234567890")
-        account2 = CompanyAccount("biedronka", "0987654321")
-        account.balance = 10
-        account.make_express_transfer(10, account2)
-        assert account.balance == -5.0
+    @pytest.mark.parametrize(
+        "balance1,balance2,expected1,expected2",
+        [(100, 100, 45, 150), (20, 0, 20, 0), (50, 0, -5, 50)],
+        ids=[
+            "regular_express_transfer",
+            "insufficnient_balance_express",
+            "transfer_all_express",
+        ],
+    )
+    def test_express_transfer(
+        self, balance1, balance2, expected1, expected2, account, account2
+    ):
+        account.balance = balance1
+        account2.balance = balance2
+        account.make_express_transfer(50, account2)
+        assert account.balance == expected1 and account2.balance == expected2
 
-    def test_account_history(self):
-        account = CompanyAccount("żabka", "1234567890")
-        account2 = CompanyAccount("biedronka", "0987654321")
+    def test_account_history(self, account, account2):
         account.receive_transfer(500)
         account.make_transfer(300, account2)
         account.make_express_transfer(100, account2)
--- tests/unit/test_personal_account_create.py
+++ tests/unit/test_personal_account_create.py
@@ -1,56 +1,81 @@
 from src.PersonalAccount import PersonalAccount
 import re
+import pytest
 
 
 class TestPersonalAccount:
+    @pytest.fixture
+    def account(self):
+        account = PersonalAccount("John", "Doe", "05280199999", "PROM_123")
+        return account
+
+    @pytest.fixture
+    def account2(self):
+        account2 = PersonalAccount("John", "Doe", "05280199999", "PROM_123")
+        return account2
+
     def test_account_creation(self):
         account = PersonalAccount("John", "Doe", "05080101397", "PROM_123")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.pesel == "05080101397"
 
-    def test_PESEL_validation(self):
-        account = PersonalAccount("John", "Doe", "05080101397", "PROM_123")
-        assert len(account.pesel) == 11 or account.pesel == "Invalid"
+    @pytest.mark.parametrize(
+        "PESEL,expected",
+        [("05280199999", "05280199999"), ("123", "Invalid")],
+        ids=["valid_pesel", "invalid_pesel"],
+    )
+    def test_PESEL_validation(self, PESEL, expected):
+        account = PersonalAccount("John", "Doe", PESEL, "PROM_123")
+        assert account.pesel == expected
 
-    def test_PESEL_invalid(self):
-        account = PersonalAccount("John", "Doe", "123", "PROM_123")
-        assert account.pesel == "Invalid"
+    @pytest.mark.parametrize(
+        "PESEL,prom,expected",
+        [
+            ("05280101397", "PROM_123", 50),
+            ("05280101397", "PROM_12", 0),
+            ("05880101397", "PROM_123", 0),
+            ("05480101397", "PROM_123", 50),
+            ("05480101397", "PROM_12", 0),
+            ("05680101397", "PROM_123", 50),
+            ("05680101397", "PROM_12", 0),
+        ],
+        ids=[
+            "promo_2000_valid",
+            "promo_200_invalid",
+            "promo_1800",
+            "promo_2100_valid",
+            "promo_2100_invalid",
+            "promo_2200_valid",
+            "promo_2200_invalid",
+        ],
+    )
+    def test_promo_validation(self, PESEL, prom, expected):
+        account = PersonalAccount("John", "Doe", PESEL, prom)
+        assert account.balance == expected
 
-    def test_promo_validation_2000(self):
-        account = PersonalAccount("John", "Doe", "05280101397", "PROM_123")
-        assert account.promo_code == "PROM_123"
-        assert re.search("^PROM_.{3}$", account.promo_code) != None
+    @pytest.mark.parametrize(
+        "balance1,balance2,expected1,expected2",
+        [(100, 100, 49, 150), (20, 0, 20, 0), (50, 0, -1, 50)],
+        ids=[
+            "regular_express_transfer",
+            "insufficnient_balance_express",
+            "transfer_all_express",
+        ],
+    )
+    def test_express_transfer(
+        self, balance1, balance2, expected1, expected2, account, account2
+    ):
+        account.balance = balance1
+        account2.balance = balance2
+        account.make_express_transfer(50, account2)
+        assert account.balance == expected1 and account2.balance == expected2
 
-    def test_promo_validation_1800(self):
-        account = PersonalAccount("John", "Doe", "05880101397", "PROM_123")
-        assert account.promo_code == "Invalid"
-
-    def test_promo_validation_2100(self):
-        account = PersonalAccount("John", "Doe", "05480101397", "PROM_123")
-        assert account.balance == 50.0
-        account = PersonalAccount("John", "Doe", "05480101397", "PROM_12")
-        assert account.balance == 0.0
-
-    def test_promo_validation_2200(self):
-        account = PersonalAccount("John", "Doe", "05680101397", "PROM_123")
-        assert account.balance == 50.0
-        account = PersonalAccount("John", "Doe", "05680101397", "PRRASDAS")
-        assert account.balance == 0.0
-
-    def test_make_express_transfer(self):
-        account = PersonalAccount("John", "Doe", "05280101397", "PROM_123")
-        account2 = PersonalAccount("Jane", "Doe", "05280101397", "PROM_123")
-        account.balance = 10.1
-        account.make_express_transfer(10.1, account2)
-        assert account.balance == -1.0
-
-    def test_account_history(self):
-        account = PersonalAccount("John", "Doe", "05280101397", "PROM_13")
-        account2 = PersonalAccount("Jane", "Doe", "05280101397", "PROM_13")
+    def test_account_history(self, account, account2):
         account.receive_transfer(500)
-        account.make_transfer(300, account2)
+        account.make_transfer(350, account2)
         account.make_express_transfer(100, account2)
         account.make_transfer(100, account2)
 
-        assert account.history == [500, -300, -100, -1]
+        assert account.balance == 99
+        assert account.history == [500, -350, -100, -1]
